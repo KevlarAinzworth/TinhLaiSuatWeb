9aa2abf1a865eb9fe34d6a32226dbb8cf30989c5
NguyenKiet <NguyenKiet@DESKTOP-7S7DD11>
2023-09-10 18:07:32 +0700

    Add project files.

---
+++ Controllers/InterestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TinhLaiSuatWeb.Models;
+
+namespace TinhLaiSuatWeb.Controllers
+{
+    public class InterestController : Controller
+    {
+        [HttpGet]
+        public IActionResult InterestCal()
+        {
+            var model = new InterestModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult InterestCal(InterestModel input)
+        {
+            var result = new InterestModel((int)input.Money, (int)input.InterestRate, (int)input.Time);
+            return View(result);
+        }
+    }
+}
---
+++ Models/InterestModel.cs
@@ -0,0 +1,25 @@
+﻿namespace TinhLaiSuatWeb.Models
+{
+    public class InterestModel
+    {
+        public double Money { get; set; }
+        public double InterestRate { get; set; }
+        public double Time { get; set; }
+        public double Total { get; set; }
+        public double Interest { get; set; }
+        public double InterestConvert { get; set; }
+
+        public InterestModel() { }
+
+        public InterestModel(int moneyInput, int interestInput, int timeInput)
+        {
+            this.Money = moneyInput;
+            this.InterestRate = interestInput;
+            this.Time = timeInput;
+            this.InterestConvert = InterestRate/100;
+            this.Interest = Money * InterestConvert/12 * Time;
+            this.Total = Interest + Money;
+            
+        }
+    }
+}
